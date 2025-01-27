1471feed9a968455d528fa33c3ea040ab875ff54
MiliAxe <miliaxe0@gmail.com>
2025-01-27 17:50:38 +0330

    remove unnecessary memory allocation for thread stacks in deadlock test

--- test_deadlock.c
+++ test_deadlock.c
@@ -43,22 +43,11 @@ void thread2(void* arg1, void* arg2) {
 }
 
 int main() {
-    void* stack1 = malloc(4096);
-    void* stack2 = malloc(4096);
-
-    if (stack1 == 0 || stack2 == 0) {
-        printf(1, "Failed to allocate memory for thread stacks\n");
-        exit();
-    }
-
     thread_create(&thread1, 0, 0);
     thread_create(&thread2, 0, 0);
 
     join(1);
     join(2);
-
-    free(stack1);
-    free(stack2);
 
     exit();
 }
